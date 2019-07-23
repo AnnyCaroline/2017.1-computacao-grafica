5bc413fa62163c5fb82f6729a30deec98ce58c15
AnnyCaroline <annycarolinegnr@gmail.com>
2019-07-22 20:00:00 -0600

    update README.md

--- trabs/trab4/trab4-leo/Segunda Parte - Execucao.m
+++
@@ -1,9 +0,0 @@
-quadrado = [1 4 4 1,
-            1 1 4 4,
-            1 1 1 1 ];
-      
-hexagono = [1 2 3 4 3 2,
-            2 1 1 2 3 3,
-            1 1 1 1 1 1 ];
-
-animarFigura(quadrado);
--- trabs/trab5/trab5-leo/Versão Inicial/printaQuadrilatero.m
+++
@@ -1,7 +0,0 @@
-function printaQuadrilatero(qua)
-  figure;
-  line (qua(1,1:2), qua(2,1:2), "linestyle", "-", "color", "b");
-  line (qua(1,2:3), qua(2,2:3), "linestyle", "-", "color", "b");
-  line (qua(1,3:4), qua(2,3:4), "linestyle", "-", "color", "b");
-  line ([qua(1,4) qua(1,1)] , [qua(2,4) qua(2,1)], "linestyle", "-", "color", "b");
-endfunction
